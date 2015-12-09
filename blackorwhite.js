2be78f68e65b9ef1c5b3b24fb8d1d90bb3f5b177
GreenWind94 <thanhphongq@gmail.com>
2015-12-09 10:34:05 +0700

    lan cuoi

--- blackorwhite.js
+++ blackorwhite.js
@@ -2,10 +2,7 @@ $(document).ready(function(){
   trainer.changeColor();
   $("#progress-box").hide();
   $("#testing-box").hide();
-  $("#code-box").hide();
-
-  // only show nn and yiq
-  $("#wcag-swatch-box").hide();
+  $("#code-box").hide();  
   $("#test-box").hide();
 });
 
@@ -37,7 +34,6 @@ var trainer = {
 
     this.changeColor();
 
-    // show the "Train network" button after we've selected a few entries
     if (this.data.length == 5) {
       $("#test-box").show();
     }
@@ -108,8 +104,6 @@ var tester = {
 
     var color = utils.normalize(color);
     $("#nn-swatch").css("color", nnColor(color));
-    $("#wcag-swatch").css("color", wcagColor(color));
-    $("#yiq-swatch").css("color", yiqColor(color));
   },
 
   viewCode : function(type) {
@@ -122,25 +116,7 @@ var tester = {
       $("#nn-swatch-box").addClass("selected");
       $("#code-box").show();
     }
-    else if(type == 'wcag' && !$("#wcag-swatch-box").hasClass("selected")) {
-      $("#code-header").text("luminosity algorithm code:");
-      var code = "var textColor = " + wcagColor.toString()
-                  + "\n\nvar contrast = " + contrast.toString()
-                  + "\n\nvar luminosity = " + luminosity.toString();
-      $("#code").text(code);
-      $(".swatch-box").removeClass("selected");
-      $("#wcag-swatch-box").addClass("selected");
-      $("#code-box").show();
-    }
-    else if(type == 'yiq' && !$("#yiq-swatch-box").hasClass("selected")) {
-      $("#code-header").text("YIQ formula code:");
-      var code = "var textColor = " + yiqColor.toString();
-
-      $("#code").text(code);
-      $(".swatch-box").removeClass("selected");
-      $("#yiq-swatch-box").addClass("selected");
-      $("#code-box").show();
-    }
+    
     else {
       $("#code-box").hide();
       $(".swatch-box").removeClass("selected");
@@ -148,45 +124,10 @@ var tester = {
   }
 }
 
-
-/* these functions are outside so we can just call toString() for 'view code'*/
 var nnColor = function(bgColor) {
   var output = runNetwork(bgColor);
   if (output.black > .5) {
     return 'black';
   }
   return 'white';
-}
-
-var wcagColor = function(bgColor) {
-  if(contrast(bgColor, {r: 1, g: 1, b: 1})
-      > contrast(bgColor, {r: 0, g: 0, b: 0}))
-    return 'white';
-  return 'black';
-}
-
-var luminosity = function(color) {
-  var r = color.r, g = color.g, b = color.b;
-  var red = (r <= 0.03928) ? r / 12.92 : Math.pow(((r + 0.055)/1.055), 2.4);
-  var green = (g <= 0.03928) ? g / 12.92 : Math.pow(((g + 0.055)/1.055), 2.4);
-  var blue = (b <= 0.03928) ? b / 12.92 : Math.pow(((b + 0.055)/1.055), 2.4);
-
-  return 0.2126 * red + 0.7152 * green + 0.0722 * blue;
-}
-
-var contrast = function(color1, color2) {
-  var lum1 = luminosity(color1);
-  var lum2 = luminosity(color2);
-  if (lum1 > lum2) {
-    return (lum1 + 0.05) / (lum2 + 0.05);
-  }
-  return (lum2 + 0.05) / (lum1 + 0.05);
-}
-
-var yiqColor = function(bgColor) {
-  var r = bgColor.r * 255,
-      g = bgColor.g * 255,
-      b = bgColor.b * 255;
-  var yiq = (r * 299 + g * 587 + b * 114) / 1000;
-  return (yiq >= 128) ? 'black' : 'white';
 }
